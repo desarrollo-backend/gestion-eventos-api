3cecf58f863650a76c05c75e4059d3b6409f8341
ceciga <cecigallardo@gmail.com>
2026-08-24 12:20:11 -0300

    Actualizar controladores y validaciones

--- src/routes/eventos.routes.js
+++ src/routes/eventos.routes.js
@@ -8,45 +8,14 @@ import {
     eliminarEvento
 } from "../controllers/eventos.controller.js";
 
-import {
-    validarDatosEvento,
-    validarIdEvento
-} from "../validators/eventos.validator.js";
-
-import { controlarErroresValidacion } from "../middlewares/validarSolicitud.js";
-
 const router = express.Router();
 
 // En Express, las rutas se evalúan en orden. Las rutas más específicas van antes, y las rutas dinámicas como /eventos/:id van después.
 router.get("/", obtenerEventos);
 router.get("/filtrados", obtenerEventosFiltrados);
-router.post(
-    "/",
-    validarDatosEvento,
-    controlarErroresValidacion,
-    crearEvento
-);
-
-router.get(
-    "/:id",
-    validarIdEvento,
-    controlarErroresValidacion,
-    obtenerEventoPorId
-);
-
-router.put(
-    "/:id",
-    validarIdEvento,
-    validarDatosEvento,
-    controlarErroresValidacion,
-    actualizarEvento
-);
-
-router.delete(
-    "/:id",
-    validarIdEvento,
-    controlarErroresValidacion,
-    eliminarEvento
-);
+router.post("/", crearEvento);
+router.get("/:id", obtenerEventoPorId);
+router.put("/:id", actualizarEvento);
+router.delete("/:id", eliminarEvento);
 
 export default router;
